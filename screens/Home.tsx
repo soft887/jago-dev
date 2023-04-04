b00bad84e4319bb1d3245421143a9ca329a8d0bd
Mugisha Precieux <precieuxmugisha@gmail.com>
2023-04-04 10:40:07 +0200

    [feat]: Add infinite scroll for home.tsx

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -5,20 +5,65 @@ import {
   StyleSheet,
   Text,
   View,
+  ActivityIndicator,
 } from "react-native";
 import { useState } from "react";
 import { EvilIcons, Feather } from "@expo/vector-icons";
 import { TextInput } from "react-native";
-import { width } from "../constants/Layout";
-import styles from "./styles/home.style";
-import ListComp from "../components/ListComp";
+import styles from "./screens/styles/home.style";
 import AddModal from "../components/AddModal";
+import ListComp from "../components/ListComp";
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(new Array(10).fill(0));
+
+  const loadData = () => {
+    setIsLoading(true);
+    // fetch data from API or local storage
+    // for example, using the Fetch API:
+    const newData = new Array(5).fill(0);
+    setTimeout(() => {
+      setData([...data, ...newData]);
+    }, 2000);
+    setIsLoading(false);
+  };
+
+  const RenderFooter = () => {
+    if (!isLoading) return null;
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          backgroundColor: "red",
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: 999,
+        }}
+      >
+        <Text>Footer</Text>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       {showModal && <AddModal setShowModal={setShowModal} />}
+      { isLoading && <View
+        style={{
+          paddingVertical: 20,
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          zIndex: 999,
+        }}
+      >
+        <ActivityIndicator animating size="large" />
+      </View>}
       <View style={styles.navCont}>
         <Image
           width={40}
@@ -56,9 +101,12 @@ const Home = () => {
         style={{
           marginTop: 3,
         }}
-        data={new Array(10).fill(0)}
+        data={data}
         keyExtractor={(item, i) => i.toString()}
         renderItem={ListComp}
+        onEndReached={loadData}
+        onEndReachedThreshold={0.5}
+        // ListFooterComponent={renderFooter}
       />
     </View>
   );
